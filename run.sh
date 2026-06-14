b4b279ef1dac57e07c598a3f06c9f69d9648ebad
Mikhail Prokhorov <100438882+Micorest@users.noreply.github.com>
2026-06-14 17:00:50 +0000

    Reporter docker и изменение run.sh

--- run.sh
+++ run.sh
@@ -12,16 +12,26 @@ elif [ "$COMMAND" == "run_generator" ]; then
     docker run --rm -v "$(pwd)/data:/data" generator
 
 elif [ "$COMMAND" == "create_local_data" ]; then
-    echo "Запускаем генератор локально..."
+    echo "Запуск генератора локально"
     mkdir -p local_data
     python3 generate.py local_data
+
+elif [ "$COMMAND" == "build_reporter" ]; then
+    echo "Сборка образа аналитика (reporter)"
+    docker build -t reporter -f Dockerfile.reporter .
+
+elif [ "$COMMAND" == "run_reporter" ]; then
+    echo "Запуск аналитика в контейнере"
+    mkdir -p data
+    docker run --rm -v "$(pwd)/data:/data" reporter
 
 elif [ "$COMMAND" == "help" ]; then
     echo "Поддерживаются команды:"
     echo "./run.sh build_generator"
     echo "./run.sh run_generator"
     echo "./run.sh create_local_data"
-
+    echo "./run.sh build_reporter"
+    echo "./run.sh run_reporter"
 else
     echo "Неизвестная команда. Напишите параметр-команду help."
 fi
